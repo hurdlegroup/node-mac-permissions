483ef75cd39a42af3328aa1039335a8384821654
Daniel McAssey <daniel.mcassey@gmail.com>
2024-11-05 02:06:29 +0000

    feat(build): only allow it to be built on MacOS

--- install-debug.js
+++
@@ -1,9 +0,0 @@
-const os = require('os');
-const {spawnSync} = require('child_process');
-
-if (os.platform() === 'darwin') {
-    spawnSync('npm', ['run', 'build-debug'], {
-        input: 'darwin detected. Build native module.',
-        stdio: 'inherit'
-    });
-}
